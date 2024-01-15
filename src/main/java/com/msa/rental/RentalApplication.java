fbc44d4ec956845f352d56949d17671068994af9
Alex <kpmyung@gmail.com>
2024-01-16 01:11:39 +0900

    feat: 프로젝트를 세팅한다

---
+++ src/main/java/com/msa/rental/RentalApplication.java
@@ -0,0 +1,14 @@
+package com.msa.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(RentalApplication.class, args);
+    }
+
+
+}
